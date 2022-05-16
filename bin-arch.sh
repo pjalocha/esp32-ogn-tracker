12bf810436f7e2f9d82260d1f4f257b796ad7dd6
Pawel Jalocha <Pawel.Jalocha@gmail.com>
2022-05-16 23:15:00 +0100

    Add Windows flashing files and create .zip for binary archive

--- bin-arch.sh
+++ bin-arch.sh
@@ -1,3 +1,6 @@
-tar cvzf esp32-ogn-tracker-bin.tgz flash_USB0.sh flash_ACM0.sh esptool.py \
+tar cvzf esp32-ogn-tracker-bin.tgz flash_USB0.sh flash_ACM0.sh esptool.py flash_COM?.bat esptool.py \
+build/partitions.bin build/bootloader/bootloader.bin build/esp32-ogn-tracker.bin \
+utils/read_log utils/serial_dump main/config.h
+zip -u esp32-ogn-tracker-bin.zip flash_USB0.sh flash_ACM0.sh esptool.py flash_COM?.bat esptool.py \
 build/partitions.bin build/bootloader/bootloader.bin build/esp32-ogn-tracker.bin \
 utils/read_log utils/serial_dump main/config.h
---
+++ flash_COM6.bat
@@ -0,0 +1,1 @@
+python esptool.py --chip esp32 --port COM6 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 build/bootloader/bootloader.bin 0x10000 build/esp32-ogn-tracker.bin 0x8000 build/partitions.bin
